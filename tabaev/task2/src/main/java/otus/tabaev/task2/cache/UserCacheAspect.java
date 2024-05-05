5f9d479bf785c08efbf2b7859860630566f191a8
Iskander <tab_is@mail.ru>
2024-05-05 14:04:00 +0300

    Fix OutOfMemoryException

--- tabaev/task2/src/main/java/otus/tabaev/task2/cache/UserCacheAspect.java
+++ tabaev/task2/src/main/java/otus/tabaev/task2/cache/UserCacheAspect.java
@@ -1,5 +1,7 @@
 package otus.tabaev.task2.cache;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -7,24 +9,28 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import otus.tabaev.task2.controller.dto.CreateUserRequest;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 @Aspect
 @Component
 public class UserCacheAspect {
 
-    private Map<String, UserDetails> userCache = new ConcurrentHashMap<>();
+
+    private Cache<String, UserDetails> userCache = Caffeine
+            .newBuilder()
+            .expireAfterWrite(30, TimeUnit.MINUTES)
+            .maximumSize(1000)
+            .build();
 
     @Before("execution(* otus.tabaev.task2.service.MyUserService.createUser(..)) && args(request)")
     public void beforeCreateUser(CreateUserRequest request) {
-        if (userCache.containsKey(request.getLogin())) {
+        if (userCache.getIfPresent(request.getLogin()) != null) {
             throw new IllegalArgumentException("User with username: " + request.getLogin() + " already exists.");
         }
     }
 
     @AfterReturning(value = "execution(* otus.tabaev.task2.service.MyUserService.createUser(..))", returning = "userDetails")
-    public void AfterCreateUser(UserDetails userDetails){
+    public void AfterCreateUser(UserDetails userDetails) {
         userCache.put(userDetails.getUsername(), userDetails);
     }
 
