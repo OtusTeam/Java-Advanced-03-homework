a688d06f0e0bd1d13dceec4612109588b57ef626
oznobishina <ksenyakom@gmail.com>
2024-05-19 03:30:29 -0700

    2 commit-fix error

--- oznobishina/memory-dump/src/main/java/com/ex/memorydump/service/CustomerService.java
+++ oznobishina/memory-dump/src/main/java/com/ex/memorydump/service/CustomerService.java
@@ -5,36 +5,33 @@ import com.ex.memorydump.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class CustomerService {
 
-    private Map<Long, List<Long>> cache = new ConcurrentHashMap<>();
+    private Map<Long, Long> cache = new ConcurrentHashMap<>();
 
     @Autowired
     private CustomerRepository customerRepository;
 
     public Customer findCustomer(String login, String password) {
-           var customer = customerRepository.findOne(login, password);
+        var customer = customerRepository.findOne(login, password);
 
-           if (customer != null) {
-               loadSmthHeavyInMemory();
-           }
+        if (customer != null) {
+            loadSmthHeavyInMemory();
+        }
 
-           return customer;
+        return customer;
     }
 
     private void loadSmthHeavyInMemory() {
-        List<Long> list = new ArrayList<>();
+        Map<Long, Long> map = new ConcurrentHashMap<>();
 
         for (int i = 0; i < 100000; i++) {
-            list.add((long) i);
+            map.put((long) i, (long) i);
         }
-        cache.put((long) cache.size(), list);
+        cache = map;
     }
 }
