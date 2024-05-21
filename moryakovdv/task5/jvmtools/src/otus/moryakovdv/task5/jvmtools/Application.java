8e963b97581e00284a591cebc7bacfcd0ae46654
moryakovdv <moryakovdv@gmail.com>
2024-05-21 15:57:09 +0300

    task5 implementation

---
+++ moryakovdv/task5/jvmtools/src/otus/moryakovdv/task5/jvmtools/Application.java
@@ -0,0 +1,15 @@
+package otus.moryakovdv.task5.jvmtools;
+
+import java.util.logging.Logger;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		
+		Logger.getLogger("TASK5").info("Task5 Application started "+((args.length>0)?args[0]:""));
+		
+		
+		
+	}
+
+}
