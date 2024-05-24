741fd213ae21298a33166aab63a3e24f9730d65f
moryakovdv <moryakovdv@gmail.com>
2024-05-24 09:26:37 +0300

    PR comments implemented

--- moryakovdv/task4/service/src/main/java/otus/moryakovdv/task4/service/WeatherService.java
+++ moryakovdv/task4/service/src/main/java/otus/moryakovdv/task4/service/WeatherService.java
@@ -4,37 +4,36 @@ import otus.moryakovdv.task4.core.Core;
 import otus.moryakovdv.task4.provider.WeatherProvider;
 import otus.moryakovdv.task4.weatherdata.WeatherConditions;
 
-
-/**Преобразовывает данные от модуля Provider в WeatherConditions и передает в CORE для записи в хранилище
+/**
+ * Преобразовывает данные от модуля Provider в WeatherConditions и передает в
+ * CORE для записи в хранилище
  */
 public class WeatherService {
-	
+
 	private Core coreModule = new Core();
 	private WeatherProvider provider = new WeatherProvider();
 
-	
-	/**сохранение погодных условий
-	 *  @param conditions - принятый от модуля Provider идентификатор погодных условий 
-	 * 	@return - идентификатор созраненной сущности
-	 * */
-	public  long saveCurrentWeatherConditions(int conditions) {
+	/**
+	 * сохранение погодных условий
+	 * 
+	 * @param conditions - принятый от модуля Provider идентификатор погодных
+	 *                   условий
+	 * @return - идентификатор сохраненной сущности
+	 */
+	public long saveCurrentWeatherConditions(int conditions) {
 		WeatherConditions curConditions = WeatherConditions.UNDEFINED;
-		if (conditions< WeatherConditions.values().length)
+		if (conditions < WeatherConditions.values().length)
 			curConditions = WeatherConditions.values()[conditions];
-				
+
 		return coreModule.saveCurrentConditions(curConditions);
-		
-		
-		
-	
+
 	}
-	
-	
-	/**метод для доступа к сервису извне*/
+
+	/** метод для доступа к сервису извне */
 	public long identifyAndSaveCurrentWeatherConditions() {
-		
-		return saveCurrentWeatherConditions (provider.identifyCurrentWeather());
-		
+
+		return saveCurrentWeatherConditions(provider.identifyCurrentWeather());
+
 	}
 
 }
