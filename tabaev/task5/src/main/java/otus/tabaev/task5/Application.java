73cdadc9b9752f00dd6dfb09d07095a55fc3c1a7
Iskander <tab_is@mail.ru>
2024-05-28 01:57:22 +0300

    Homework 5: Keytool Jlink

---
+++ tabaev/task5/src/main/java/otus/tabaev/task5/Application.java
@@ -0,0 +1,12 @@
+package otus.tabaev.task5;
+
+import java.util.logging.Logger;
+
+public class Application {
+
+    private static final Logger logger = Logger.getLogger(Application.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("This application was written by Iskander Tabaev");
+    }
+}
