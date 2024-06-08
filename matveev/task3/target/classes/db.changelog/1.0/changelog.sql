910909322e06e5f903e1d7bbddaa13a19c9edb18
Aleksey Matveev <a.matveev@arenadata.io>
2024-06-08 14:29:23 +0300

    Remove target dir

--- matveev/task3/target/classes/db.changelog/1.0/changelog.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE my_table (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    column1 VARCHAR(50),
-    column2 VARCHAR(50),
-    column3 VARCHAR(50),
-    column4 VARCHAR(50)
-    );
--- matveev/task3/target/classes/db.changelog/1.0/changelog2.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO my_table (column1, column2, column3, column4)
-SELECT
-    RANDOM_UUID(), -- Generating a random UUID for column1
-    RANDOM_UUID(), -- Generating a random UUID for column2
-    RANDOM_UUID(), -- Generating a random UUID for column3
-    RANDOM_UUID()  -- Generating a random UUID for column4
-FROM
-    SYSTEM_RANGE(1, 100000); -- Generating 1000 rows
