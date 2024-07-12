fe3985496486f78b2d84ea0c2f04a78cc4d2c878
Kseniya Oznobishina <kseniya.oznobishina@neo.gg>
2024-07-12 17:24:05 +0300

    1 commit - create Dockerfile and app

---
+++ oznobishina/task12/main/src/main/java/org/example/endpoint/CustomerController.java
@@ -0,0 +1,31 @@
+package org.example.endpoint;
+
+import org.example.model.Customer;
+import org.example.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Customer login(@RequestBody Customer customer) {
+        return customerService.findCustomer(customer.getLogin(), customer.getPassword());
+    }
+
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity register(@RequestBody Customer customer) {
+        customerService.saveCustomer(customer);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+}
