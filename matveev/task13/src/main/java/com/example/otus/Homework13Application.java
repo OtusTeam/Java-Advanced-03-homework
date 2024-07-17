12621d033366a4f0f1f7375f62d26ae6c4bdda30
Aleksey Matveev <a.matveev@arenadata.io>
2024-07-17 19:55:14 +0300

    Task13.Kubernetes,Helm.

---
+++ matveev/task13/src/main/java/com/example/otus/Homework13Application.java
@@ -0,0 +1,11 @@
+package com.example.otus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework13Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Homework13Application.class, args);
+	}
+}
---
+++ matveev/task13/src/main/java/com/example/otus/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.example.otus.controller;
+
+import com.example.otus.repository.UserRepository;
+import com.example.otus.models.User;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @GetMapping("")
+    public synchronized ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
+    }
+
+    @GetMapping("/helloworld")
+    public  ResponseEntity<String> getHelloWorld() {
+        return ResponseEntity.ok("Hello world!");
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        user.setPassword(user.getPassword());
+        User saveUser = userRepository.save(user);
+        System.out.println("User was created: " + user.getName());
+        return ResponseEntity.ok(saveUser);
+    }
+
+
+}
