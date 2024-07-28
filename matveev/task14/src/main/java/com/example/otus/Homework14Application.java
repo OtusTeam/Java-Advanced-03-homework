f3d60be6f2a7a8fdfb1314f066bafc473ece01b7
Aleksey Matveev <a.matveev@arenadata.io>
2024-07-28 13:14:38 +0300

    Task14.Prometheus/Grafana

---
+++ matveev/task14/src/main/java/com/example/otus/Homework14Application.java
@@ -0,0 +1,11 @@
+package com.example.otus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework14Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Homework14Application.class, args);
+	}
+}
