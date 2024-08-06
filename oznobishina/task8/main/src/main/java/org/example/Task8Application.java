439bd356d10de32753662d1c141e4339fe375cd0
Kseniya Oznobishina <kseniya.oznobishina@neo.gg>
2024-08-06 09:52:25 +0300

    1 commit - init commit

---
+++ oznobishina/task8/main/src/main/java/org/example/Task8Application.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+@EntityScan
+@SpringBootApplication
+public class Task8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task8Application.class, args);
+    }
+
+}
---
+++ oznobishina/task8/main/src/main/java/org/example/service/CustomerService.java
@@ -0,0 +1,39 @@
+package org.example.service;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.example.model.Customer;
+import org.example.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CustomerService {
+
+    public static final int ITERATIOIN_NUMBER = 1;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public Customer findCustomer(String login, String password) {
+        String hashedPassword = HashService.hash512(password, ITERATIOIN_NUMBER);
+        validate(login);
+
+        return customerRepository.findOne(login, hashedPassword);
+    }
+
+    private void validate(String login) {
+        DeadlockExample deadlock = new DeadlockExample();
+        new Thread(deadlock::operation1, "T1").start();
+        new Thread(deadlock::operation2, "T2").start();
+    }
+
+    public Customer saveCustomer(Customer customer) {
+        String hashedPassword = HashService.hash512(customer.getPassword(), ITERATIOIN_NUMBER);
+        customer.setPassword(hashedPassword);
+
+        return customerRepository.save(customer);
+    }
+}
